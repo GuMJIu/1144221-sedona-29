c9cf3495dafd13cbf98b6bd5845389d3144269df
gumjiu <leve-kirill@mail.ru>
2020-11-07 21:32:58 +0300

    1

--- js/modal.js
+++
@@ -1,7 +0,0 @@
-const search = document.querySelector(".search");
-const modal = document.querySelector(".modal-in");
-search.addEventListener("click", function (evt)
-{
-  evt.preventDefault();
-  modal.classList.add("modal-show");
-});
